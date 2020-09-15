96ac8ae4d06a7a51db00232acf0f29615a0cfd6e
rejiejay <454766952@qq.com>
2020-09-15 23:03:36 +0800

    updata start.jsx

--- views/page/start.jsx
+++ views/page/start.jsx
@@ -1,10 +1,11 @@
 import WebSocket from './module/web-socket'
+import User from './module/user'
 
 export const PageStart = {
     ref: React.createRef(),
 
     react: <ReactComponent ref={PageStart.ref} />,
-
+    
     init: async function () {
         await render(this.react)
         WebSocket.bind(this.ref)
@@ -12,23 +13,66 @@ export const PageStart = {
 }
 
 class ReactComponent extends Component {
-    constructor(props) {
+    constructor (props) {
         super(props)
 
         this.state = {
-            isEdit: this.props.isEdit,
-            value: this.props.value
         }
+
+        this.numberNoList = []
+        this.nameList = []
+    }
+
+    getNumberNoAndNameList() {
+        const fetchtInstance = await fetch.get('getNumberNoAndNameList')
+        if (fetchtInstance.result !== 1) return alert(fetchtInstance.message)
+
+        const fetchtData = fetchtInstance.data
+        this.numberNoList = fetchtData.numberNoList
+        this.nameList = fetchtData.nameList
+    }
+
+    WS_Income_Start_NumberNoList(numberNoList = []) {
+        this.numberNoList = numberNoList
+    }
+
+    WS_Income_Start_NumberNoList(nameList = []) {
+        this.nameList = nameList
+    }
+
+    startGame() {
+        const defaultNumberNo = createNumberNo(this.numberNoList)
+        const defaultName = createName(this.nameList)
+
+        const inputPopUp = ({ numberNo, name }) => popUp({ numberNo, name })
+        .then(inputHandle)
+
+        const inputHandle = async ({ numberNo, name }) => {
+            const repeatInstance = isRepeat({ numberNo, name })
+            if (repeatInstance.result !== 1) return alert(`${repeatInstance.message} 是重复的`).then(inputPopUp({ numberNo, name }))
+
+            const fetchtInstance = await fetch.post({ numberNo, name })
+            if (fetchtInstance.result !== 1) return alert(fetchtInstance.message).then(inputPopUp({ numberNo, name }))
+
+            User.initUserInfor({ numberNo, name })
+            PageReadlyComfirm.init()
+        }
+
+        inputPopUp({ defaultNumberNo, defaultName })
     }
 
     render() {
-        return
-            ;
+        return <div>
+            <WolfTitle>wolf kill</WolfTitle>
+            <Button
+                onClick={this.startGame}
+            >start game</Button>
+        </div>
     }
 }
 
 const render = component => new Promise(resolve => ReactDOM.render(component, document.body, instance => resolve(instance)))
 
 export const PageRelink = {
-    init: () => { }
+    init: () => {}
 }
